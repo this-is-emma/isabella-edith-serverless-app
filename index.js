fedc3408cf9a319ec5b1f8f61b8c4d3f30639956
ThisIsEmma <sakatia.lise@gmail.com>
2023-06-08 09:13:35 -0400

    removed key

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// serverless config credentials --provider aws --key AKIAVSYBQIVF5GUZW27M --secret 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
-
 // index.js
- 
+
 const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
- 
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
- 
+
 module.exports.handler = serverless(app);
